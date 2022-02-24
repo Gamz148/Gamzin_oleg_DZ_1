071ef04403af6412eb54851e3c0a2eb4ef3b1516
Gamz148 <97736124+Gamz148@users.noreply.github.com>
2022-02-24 05:00:11 +0200

    Add files via upload
    не смог сделать 2 задание, подскажите где брать информацию, спасибо.

---
+++ Gamzin_oleg_2.py
@@ -0,0 +1,13 @@
+# список
+
+N = 1000
+list_1 = list(range(1, N+1, 2))
+list_2 = [i**3 for i in range(1, N+1, 2)]
+print(list_1)
+print(list_2)
+
+# Список из кубов нечетных чисел
+# list_3 = [1, 3, 5, 7, 9]
+# print(list_3)
+# list_4 = [i**3 for i in list_3]
+# print(list_4)
